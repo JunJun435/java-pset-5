8f2069ea9cb643df8c1725851a3475d99939c5ac
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2020-10-06 12:06:54 -0400

    done with 1

---
+++ src/Exercise1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        input.close();
+
+        if(number > 0) {
+            System.out.println("Positive.");
+        }
+        else if(number < 0) {
+            System.out.println ("Negative");
+        }
+        else {
+            System.out.println ("Zero.");
+        }
+    }
+}
